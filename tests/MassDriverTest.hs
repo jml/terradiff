97abe1401aa3640daa11d7491a7dd0992ed04f2b
Jonathan Lange <jml@mumak.net>
2018-06-30 12:03:57 +0100

    License under AGPL v3

--- tests/MassDriverTest.hs
+++ tests/MassDriverTest.hs
@@ -1,3 +1,21 @@
+-- | Tests for mass-driver.
+--
+-- Copyright (c) 2018 Jonathan M. Lange
+--
+-- This file is part of mass-driver.
+--
+-- mass-driver is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or (at your
+-- option) any later version.
+--
+-- mass-driver is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+-- FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+-- for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with mass-driver. If not, see <https://www.gnu.org/licenses/>.
 module MassDriverTest where
 
 import Protolude
